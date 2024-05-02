b05a3c5b2e2aa54a5e1cb628134096e11f6bd8cb
Brannon Glover <brannon.glover@warnermedia.com>
2024-05-02 11:11:59 -0400

    Styling to ind services

--- src/components/individual-box.js
+++ src/components/individual-box.js
@@ -4,7 +4,8 @@ import { styled } from 'styled-components';
 const IndividualBoxWrapper = styled.div`
   display: grid;
   align-items: center;
-  grid-template-columns: 20rem 1fr;
+  grid-template-columns: 1fr 3rem;
+  border-bottom: 1px solid gray;
 
   @media screen and (min-width: 1000px) {
     grid-template-columns: 23rem 3rem;
@@ -13,18 +14,18 @@ const IndividualBoxWrapper = styled.div`
 `;
 
 const ServiceBoxName = styled.span`
-  padding: 1rem 0;
+  padding: 1.1rem 1rem;
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 19px;
-  background-color: #FFF8E8;
+  font-size: 1rem;
+  background-color: #faf7f7;
 `;
 
 const ServicePrice = styled.span`
   font-size: 19px;
   font-family: Arial, Helvetica, sans-serif;
   font-weight: 700;
-  padding: .5rem 0;
-  background-color: #E8EFFF;
+  padding: 1rem 0.5rem;
+  background-color: #cee3f0;
 `;
 
 const ServiceBox = ({ services }) => {
