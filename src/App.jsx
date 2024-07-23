9d98597a5e61d58be9f9a25d92052b81e48c1813
Brannon Glover <brannon.glover@warnermedia.com>
2024-07-23 10:55:52 -0400

    fixed deployment isue

--- src/App.jsx
+++ src/App.jsx
@@ -170,19 +170,12 @@ const App = () => {
         window.location = url;
       }
     };
+
     gtag('event', 'conversion', {
         'send_to': 'AW-16578987654/P1sJCJPchsUZEIaNveE9',
         'event_callback': callback
     });
-    return false;
-  }
 
-  function gtag_report_conversion(url) {
-    var callback = function () {
-      if (typeof (url) != 'undefined') {
-        window.location = url;
-      }
-    };
     gtag('event', 'conversion', {
       'send_to': 'TAG_ID/CONVERSION_LABEL',
       'value': 1.0,
