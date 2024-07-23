15c255fc6737c5802970f4de18c857c3101b35b4
Brannon Glover <brannon.glover@warnermedia.com>
2024-07-23 10:07:27 -0400

    added gtag to click event page

--- src/App.jsx
+++ src/App.jsx
@@ -159,6 +159,19 @@ const App = () => {
 
   function gtag_report_conversion(url) {
     var callback = function () {
+      if (typeof(url) != 'undefined') {
+        window.location = url;
+      }
+    };
+    gtag('event', 'conversion', {
+        'send_to': 'AW-16578987654/P1sJCJPchsUZEIaNveE9',
+        'event_callback': callback
+    });
+    return false;
+  }
+
+  function gtag_report_conversion(url) {
+    var callback = function () {
       if (typeof (url) != 'undefined') {
         window.location = url;
       }
