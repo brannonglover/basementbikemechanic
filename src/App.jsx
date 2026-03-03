4d3fa4c471acd21441e61df415a7cc399af5718a
Brannon Glover <brannon.glover@warnermedia.com>
2026-03-03 16:07:47 -0500

    removed unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from 'styled-components';
 import ServiceBox from "./components/service-box";
 import IndividualBox from "./components/individual-box";
